e5705f610fa781d38b3caac9f0b8453d6ef5c99d
Claire Sosset <claire.sosset@gmail.com>
2022-05-22 19:35:42 +0200

    feat: add component Heading1

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Button, Text } from "ui";
+import { Button, Text, Heading1 } from "ui";
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <Heading1>Web</Heading1>
       <Text>Welcome to the web page</Text>
       <Button>Click me!</Button>
     </div>
