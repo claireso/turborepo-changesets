4587ca6599d43c05204c00bd523b8126a6c62f90
Claire Sosset <claire.sosset@gmail.com>
2022-05-22 19:00:20 +0200

    feat: add component Text

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,10 @@
-import { Button } from "ui";
+import { Button, Text } from "ui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      <Text>Welcome to the web page</Text>
       <Button>Click me!</Button>
     </div>
   );
